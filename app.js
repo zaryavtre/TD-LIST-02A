f382db75a41c944d5ef3711e6a330c136bf53ed7
zaryavtre <von.zaryavtre@gmail.com>
2023-06-02 17:22:31 +0100

    Refactor entire app

--- app.js
+++ app.js
@@ -1,135 +1,76 @@
-const addTask = document.querySelector('.add-task')
-const overlay = document.querySelector('.blur-overlay')
-const addModal = document.querySelector('.add-modal')
-const submitBtn = document.querySelector('.submit-btn')
-const todosWrapper = document.querySelector('.todos-wrapper')
-const dateWrapper = document.querySelector('.date-render')
-
-const todosArr = []
+const todoObject = []
 
 const date = new Date()
-
 let day = date.getDate()
 let formating = new Intl.DateTimeFormat('en-us', { month: 'long' })
 let months = formating.format(new Date())
 let year = date.getFullYear()
-
 let currentDate = `${day} ${months} ${year}`
 
-todosWrapper.addEventListener('click', deleteTodo)
-todosWrapper.addEventListener('click', completeTodo)
+document.addEventListener('click', function(e) {
+    if(e.target.dataset.add) {
+        openOverlay()
+    } else if (e.target.matches('.blur-overlay')) {
+       closeOverlay()
+    } else if (e.target.matches('.submit-btn')) {
+        renderTodo()
+    }
+})
 
-addTask.addEventListener('click', openModal)
-overlay.addEventListener('click', closeModal)
-submitBtn.addEventListener('click', renderTodo)
-document.addEventListener('DOMContentLoaded', renderDateInput)
+function openOverlay() {
+    document.querySelector('.blur-overlay').classList.toggle('display-block')
+    document.querySelector('.add-modal').classList.toggle('display-block')
+    renderDateInput()
+}
+
+function closeOverlay() {
+    document.querySelector('.blur-overlay').classList.toggle('display-block')
+    document.querySelector('.add-modal').classList.toggle('display-block')
+}
 
 function renderDateInput() {
-  dateWrapper.innerHTML = `
- <input type="date" id="due-date-input" value="" min />`
-  document.getElementById('due-date-input').valueAsDate = new Date()
+    const dateWrapper = document.querySelector('.date-render')
+    dateWrapper.innerHTML = `
+   <input type="date" id="due-date-input" value="" min />`
+    document.getElementById('due-date-input').valueAsDate = new Date()
 }
 
-function openModal() {
-  overlay.classList.toggle('display-block')
-  addModal.classList.toggle('display-block')
-}
+function todoTemplate(todoObject) {
+    let templateTodo = ''
+   const inputValue = document.querySelector('input[type="text"]').value
+   const dateValue = document.querySelector('input[type="date"]').value
 
-function closeModal() {
-  overlay.classList.toggle('display-block')
-  addModal.classList.toggle('display-block')
+   const values = [{
+    textInput: inputValue,
+    dateInput: dateValue 
+   }]
+
+   todoObject.push(values[0])
+
+   const index = todoObject.length - 1
+   console.log(todoObject)
+   todoObject.forEach((todo) => {
+        templateTodo += `
+        <div class="todo-wrapper" id="todo-${index}"  data-index="${index}">
+            <div class="todo-content">
+                <div class="round">
+                    <input type="checkbox" id="${todo.textInput}" class="completed">
+                    <label for="${todo.textInput}"></label>
+                </div>  
+            <div class="p-wrapper">
+                <h2>${todo.dateInput}</h2>
+                <p class="todo-p">${todo.textInput}</p>
+            </div>
+        </div>
+        <div class="delete-wrapper">
+            <button class="delete" data-index="${index}"></button>
+        </div>
+    </div>`
+    })   
+   return templateTodo
 }
 
 function renderTodo() {
-  let theTodo = document.querySelector('input[type="text"]')
-  let valueDate = document.querySelector('input[type="date"]').value
-  const arrayObject = {
-    todo: theTodo.value,
-    date: valueDate,
-  }
-  todosArr.push(arrayObject)
-  let index = todosArr.length - 1 // get the index of the new todo
-  todosWrapper.innerHTML += `
-        <div class="todo-wrapper" id="todo-${index}"  data-index="${index}">
-        <div class="todo-content">
-        <div class="round">
-          <input type="checkbox" id="${theTodo.value}" class="completed">
-          <label for="${theTodo.value}"></label>
-        </div>  
-          <div class="p-wrapper">
-                <h2>${valueDate}</h2>
-                <p class="todo-p">
-                    ${theTodo.value}
-                </p>
-            </div>
-            </div>
-            <div class="delete-wrapper">
-                <button class="delete" data-index="${index}">
-                
-                </button>
-            </div>
-        </div>`
-  theTodo.value = ''
-  closeModal()
+    document.querySelector('.todos-wrapper').innerHTML = todoTemplate(todoObject)
 }
 
-function deleteTodo(e) {
-  // check if the delete button was clicked
-  if (e.target.classList.contains('delete')) {
-    let index = e.target.dataset.index
-    // remove the string from the array
-    todosArr.splice(index, 1)
-    //remove the corresponding div from the dom
-    let todoDiv = document.querySelector(`#todo-${index}`)
-    todoDiv.remove()
-    console.log(todosArr)
-  }
-}
-
-function completeTodo(e) {
-  let completeArray = []
-
-  if (e.target.matches('input[type="checkbox"]')) {
-    let todoWrapper = e.target.closest('.todo-wrapper')
-    let index = todoWrapper.getAttribute('data-index')
-    let completeArr = todosArr[index]
-    if (e.target.checked) {
-      todosArr.splice(index, 1)
-      let todoDiv = document.querySelector(`#todo-${index}`)
-      todoDiv.remove()
-      completeArray.push(completeArr)
-      console.log(completeArray)
-      todoWrapper.style.opacity = '45%'
-      //renderComplete()
-    } else {
-      todoWrapper.style.opacity = '100%'
-    }
-  }
-}
-
-function renderComplete() {
-  todosWrapper.innerHTML = `
-      <h1 class="completed-h1">Completed</h1>
-    `
-  todosWrapper.innerHTML += `
-    <div class="todo-wrapper" id="todo-${index}"  data-index="${index}">
-        <div class="todo-content">
-        <div class="round">
-          <input type="checkbox" id="${theTodo.value}" class="completed">
-          <label for="${theTodo.value}"></label>
-        </div>  
-          <div class="p-wrapper">
-                <h2>${valueDate}</h2>
-                <p class="todo-p">
-                    ${theTodo.value}
-                </p>
-            </div>
-            </div>
-            <div class="delete-wrapper">
-                <button class="delete" data-index="${index}">
-                
-                </button>
-            </div>
-        </div>
-  `
-}
---
+++ oldApp.js
@@ -0,0 +1,147 @@
+const addTask = document.querySelector('.add-task')
+const overlay = document.querySelector('.blur-overlay')
+const addModal = document.querySelector('.add-modal')
+const submitBtn = document.querySelector('.submit-btn')
+const todosWrapper = document.querySelector('.todos-wrapper')
+const dateWrapper = document.querySelector('.date-render')
+
+const todosArr = []
+
+const date = new Date()
+
+let day = date.getDate()
+let formating = new Intl.DateTimeFormat('en-us', { month: 'long' })
+let months = formating.format(new Date())
+let year = date.getFullYear()
+
+let currentDate = `${day} ${months} ${year}`
+
+todosWrapper.addEventListener('click', deleteTodo)
+todosWrapper.addEventListener('click', completeTodo)
+
+addTask.addEventListener('click', openModal)
+overlay.addEventListener('click', closeModal)
+submitBtn.addEventListener('click', renderTodo)
+document.addEventListener('DOMContentLoaded', renderDateInput)
+
+function renderDateInput() {
+  dateWrapper.innerHTML = `
+ <input type="date" id="due-date-input" value="" min />`
+  document.getElementById('due-date-input').valueAsDate = new Date()
+}
+
+function openModal() {
+  overlay.classList.toggle('display-block')
+  addModal.classList.toggle('display-block')
+}
+
+function closeModal() {
+  overlay.classList.toggle('display-block')
+  addModal.classList.toggle('display-block')
+}
+
+function renderTodo() {
+  let theTodo = document.querySelector('input[type="text"]')
+  let valueDate = document.querySelector('input[type="date"]').value
+  const arrayObject = {
+    todo: theTodo.value,
+    date: valueDate,
+  }
+  todosArr.push(arrayObject)
+  let index = todosArr.length - 1 // get the index of the new todo
+
+  todosWrapper.innerHTML += `
+        <div class="todo-wrapper" id="todo-${index}"  data-index="${index}">
+        <div class="todo-content">
+        <div class="round">
+          <input type="checkbox" id="${theTodo.value}" class="completed">
+          <label for="${theTodo.value}"></label>
+        </div>  
+          <div class="p-wrapper">
+                <h2>${valueDate}</h2>
+                <p class="todo-p">
+                    ${theTodo.value}
+                </p>
+            </div>
+            </div>
+            <div class="delete-wrapper">
+                <button class="delete" data-index="${index}">
+                
+                </button>
+            </div>
+        </div>`
+  theTodo.value = ''
+  closeModal()
+}
+
+function deleteTodo(e) {
+  // check if the delete button was clicked
+  if (e.target.classList.contains('delete')) {
+    let index = e.target.dataset.index
+    // remove the string from the array
+    todosArr.splice(index, 1)
+    //remove the corresponding div from the dom
+    let todoDiv = document.querySelector(`#todo-${index}`)
+    todoDiv.remove()
+    console.log(todosArr)
+  }
+}
+
+function completeTodo(e) {
+  let completeArray = []
+
+  /*const mapTodos = todosArr.map(function(complete) {
+    return complete.index
+  }).indexOf(checked)
+
+  if(e.target.checked) {
+    completeArray.push(mapTodos)
+    let divCompleted = document.querySelector(`#todo-${checked}`)
+    divCompleted.remove()
+
+  }*/
+
+  if (e.target.matches('input[type="checkbox"]')) {
+    let todoWrapper = e.target.closest('.todo-wrapper')
+    let index = todoWrapper.getAttribute('data-index')
+    let completeArr = todosArr[index]
+    if (e.target.checked) {
+      todosArr.splice(index, 1)
+      let todoDiv = document.querySelector(`#todo-${index}`)
+      todoDiv.remove()
+      completeArray.push(completeArr)
+      console.log(completeArray)
+      todoWrapper.style.opacity = '45%'
+      renderComplete()
+    } else {
+      todoWrapper.style.opacity = '100%'
+    }
+  }
+}
+
+function renderComplete() {
+  todosWrapper.innerHTML = `
+      <h1 class="completed-h1">Completed</h1>
+    `
+  todosWrapper.innerHTML += `
+    <div class="todo-wrapper" id="todo-${index}"  data-index="${index}">
+        <div class="todo-content">
+        <div class="round">
+          <input type="checkbox" id="${theTodo.value}" class="completed">
+          <label for="${theTodo.value}"></label>
+        </div>  
+          <div class="p-wrapper">
+                <h2>${valueDate}</h2>
+                <p class="todo-p">
+                    ${theTodo.value}
+                </p>
+            </div>
+            </div>
+            <div class="delete-wrapper">
+                <button class="delete" data-index="${index}">
+                
+                </button>
+            </div>
+        </div>
+  `
+}
